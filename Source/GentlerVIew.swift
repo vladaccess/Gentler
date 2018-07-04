45dcea13f477460ba5f74de5aaec12b2a671b6de
Vladislav <vladaccess@outlook.com>
2018-07-04 20:04:12 +0300

    Initial commit

---
+++ Source/GentlerVIew.swift
@@ -0,0 +1,15 @@
+//
+//  GentlerVIew.swift
+//  Gentler
+//
+//  Created by Vladislav on 04.07.2018.
+//
+
+import Foundation
+import UIKit
+
+
+
+class GentlerView:UIView {
+    
+}
---
+++ Source/Pointer.swift
@@ -0,0 +1,32 @@
+//
+//  Pointer.swift
+//  Gentler
+//
+//  Created by Vladislav on 04.07.2018.
+//
+
+import Foundation
+
+enum Pointer {
+    case left
+    case right
+    case bottom
+    case top
+    case top_left
+    case top_right
+    case bottom_left
+    case bottom_right
+    
+    public var pointer:CGPoint {
+        switch self {
+        case .top: return CGPoint(x: 0.5, y: 0)
+        case .left: return CGPoint(x: 0, y: 0.5)
+        case .bottom: return CGPoint(x: 0.5, y: 1)
+        case .right: return CGPoint(x: 1, y: 0.5)
+        case .top_left: return CGPoint(x: 0, y: 0)
+        case .top_right: return CGPoint(x: 1, y: 0)
+        case .bottom_left: return CGPoint(x: 0, y: 1)
+        case .bottom_right: return CGPoint(x: 1, y: 1)
+        }
+    }
+}
